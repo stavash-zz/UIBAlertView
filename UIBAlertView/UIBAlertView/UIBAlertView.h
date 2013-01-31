c8be7b07143742474d34b02a9a6e1cf1d79c03fc
Stav Ashuri <stavash@gmail.com>
2013-02-01 00:07:54 +0200

    Added requirement for nil termination

--- UIBAlertView/UIBAlertView/UIBAlertView.h
+++ UIBAlertView/UIBAlertView/UIBAlertView.h
@@ -12,7 +12,7 @@ typedef void (^AlertDismissedHandler) (NSInteger selectedIndex, BOOL didCancel);
 
 @interface UIBAlertView : NSObject
 
-- (id)initWithTitle:(NSString *)aTitle message:(NSString *)aMessage cancelButtonTitle:(NSString *)aCancelTitle otherButtonTitles:(NSString *)otherTitles,...;
+- (id)initWithTitle:(NSString *)aTitle message:(NSString *)aMessage cancelButtonTitle:(NSString *)aCancelTitle otherButtonTitles:(NSString *)otherTitles,...NS_REQUIRES_NIL_TERMINATION;
 - (void)showWithDismissHandler:(AlertDismissedHandler)handler;
 
 @end
