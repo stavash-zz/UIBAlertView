abc2f882d5cc50f361fd4c535983989562c8673c
Joseph Toronto <j.toronto@icloud.com>
2014-06-08 10:16:38 -0400

    fixed iOS8 deallocated instance crash.
    Added class property to prevent the alert view from being deallocated.
    Fixes crash when dismissing alert view in iOS8.

--- UIBAlertView/DemoViewController.m
+++ UIBAlertView/DemoViewController.m
@@ -10,7 +10,7 @@
 #import "UIBAlertView.h"
 
 @interface DemoViewController ()
-
+@property (nonatomic, strong) UIBAlertView *currentAlert;
 @end
 
 @implementation DemoViewController
@@ -31,6 +31,7 @@
 
 - (IBAction)activateAlert:(id)sender {
     UIBAlertView *alert = [[UIBAlertView alloc] initWithTitle:@"Alert" message:@"Testing" cancelButtonTitle:@"Cancel" otherButtonTitles:@"Button 1",@"Button 2",nil];
+    _currentAlert = alert;
     [alert showWithDismissHandler:^(NSInteger selectedIndex, NSString *selectedTitle, BOOL didCancel) {
         if (didCancel) {
             NSLog(@"User cancelled");
